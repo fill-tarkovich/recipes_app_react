17d3393a366763deb72c1fb3fd88c2d2d85ca7ef
Filipp Tarkovich <fill.tarkovich@gmail.com>
2022-06-09 14:26:10 +0300

    added single recipe page

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./../sass/card.scss";
 
-const RecipeCard = ({ title, description, img, flag }) => {
+const RecipeCard = ({ title, description, img, flag, data }) => {
   return (
     <div className="recipe_card">
       <img src={img} alt={title} />
@@ -10,7 +10,9 @@ const RecipeCard = ({ title, description, img, flag }) => {
       <h2>"{title}"</h2>
       <p>{description}</p>
       <div>
-        <Link to={title}>See recipe</Link>
+        <Link to={title} state={{ data: data, flag: flag }}>
+          See recipe
+        </Link>
       </div>
     </div>
   );
--- src/components/SingleRecipe.jsx
+++ src/components/SingleRecipe.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import "./../sass/SingleRecipe.scss";
 
-const SingleRecipe = () => {
-  return <div>Recipe will be here</div>;
+const RecipeSingle = () => {
+  const location = useLocation();
+  const flag = location.state.flag;
+  const recipe = location.state.data;
+
+  return (
+    <div className="recipe_page">
+      <h1>{recipe.title}</h1>
+      <div className="recipe">
+        <img src={recipe.img} alt={recipe.name} />
+        <div className="flag">{flag}</div>{" "}
+        <div className="info">
+          <h3>Author: {recipe.author}</h3>
+          <h3>Ingredients:</h3>
+          <ul className="ingredients_list">
+            {recipe.ingredients.map((ingredient) => (
+              <li key={ingredient.id}>
+                {ingredient.name}: {ingredient.quantity}
+              </li>
+            ))}
+          </ul>
+          <p>{recipe.description}</p>
+        </div>
+      </div>
+      <div className="instructions">
+        <h3>Preparation:</h3>
+        <p>{recipe.preparation}</p>
+      </div>
+    </div>
+  );
 };
 
-export default SingleRecipe;
+export default RecipeSingle;
